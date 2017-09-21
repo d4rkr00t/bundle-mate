673e6b618ff744d9540c50bf78cf9df454c2572c
Stanislav Sysoev <d4rkr00t@gmail.com>
2017-09-22 00:48:47 +1000

    feat: some boilerplate for bundle-mate-cli

---
+++ packages/bundle-mate-cli/cli.js
@@ -0,0 +1,77 @@
+#!/usr/bin/env node
+
+/* @flow */
+
+const meow = require("meow");
+const chalk = require("chalk");
+
+const cli = meow(
+  {
+    help: [
+      chalk.green("Usage"),
+      "  $ bm report --adapter webpack",
+      "",
+      chalk.green("Commands"),
+      `  ${chalk.yellow("report")}            Runs an analyzer to build a report for a bundle`,
+      `  ${chalk.yellow("-n, --ngrok")}       Exposes server to the real world by ngrok.`,
+      `  ${chalk.yellow("-o, --open")}        Opens web server URL in the default browser.`,
+      "",
+      chalk.green("Options"),
+      `  ${chalk.yellow("-a, --adapter")}     Adapter BundleMate will use to create a report`,
+      `  ${chalk.yellow("-v, --version")}     Shows version.`,
+      `  ${chalk.yellow("--help")}            Shows help.`
+    ]
+  },
+  {
+    alias: {
+      a: "adapter",
+      v: "version"
+    }
+  }
+);
+
+const ALLOW_COMMANDS = ["report"];
+const COMMAND_VALIDATION_RULES = {
+  report: {
+    input: false, // optional
+    flags: {
+      adapter: true // required
+    }
+  }
+};
+const [command, ...input] = cli.input || [];
+const flags = cli.flags || {};
+
+const validateCommand = (command /*: string */, input /* Array<string> */, flags /*: any */) => {
+  const validationRules = COMMAND_VALIDATION_RULES[command];
+  if (validationRules.input && (!input || !input.length)) {
+    return `Command "${command}" expect additional input, use bm --help for more info`;
+  }
+  if (validationRules.flags) {
+    const errors = Object.keys(validationRules.flags).reduce((acc, rule) => {
+      if (validationRules.flags[rule] && !flags[rule]) {
+        acc.push(`Command "${command}" expects "--${rule}" flag, use bm --help for more info`);
+      }
+      return acc;
+    }, []);
+    return errors.join("\n");
+  }
+};
+
+if (!command) {
+  console.log(cli.help);
+} else if (!ALLOW_COMMANDS.includes(command)) {
+  console.log(`Command "${command}" is not supported`);
+  process.exit(1);
+} else {
+  const commandValidationErrors = validateCommand(command, input, flags);
+
+  if (commandValidationErrors) {
+    console.log(commandValidationErrors);
+    process.exit(1);
+  }
+
+  console.log("command: ", command);
+  console.log("input: ", input);
+  console.log("flags: ", flags);
+}
